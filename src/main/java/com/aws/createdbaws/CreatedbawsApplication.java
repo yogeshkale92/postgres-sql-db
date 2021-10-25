738ff006e92dc11cf98b54a6fe51988e1e724c1d
yogeshkale92 <yogeshkale92@gmail.com>
2021-10-25 12:50:15 +0530

    test

--- src/main/java/com/aws/createdbaws/CreatedbawsApplication.java
+++ src/main/java/com/aws/createdbaws/CreatedbawsApplication.java
@@ -15,7 +15,7 @@ public class CreatedbawsApplication {
 
         SpringApplication.run(CreatedbawsApplication.class, args);
 
-        Connection c = DriverManager.getConnection("jdbc:postgresql://database-1.cmeqgqyduags.us-west-2.rds.amazonaws.com:5432/", "postgres22", "nepr12345");
+        Connection c = DriverManager.getConnection("jdbc:postgresql://database-1.cmeqgqyduags.us-west-2.rds.amazonaws.com:5432/postgres", "postgres22", "nepr12345");
         Statement statement = c.createStatement();
         statement.executeUpdate("CREATE DATABASE sales");
     }
